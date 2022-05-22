96b9766969414b22eaf9959548d4e297c21336af
Nirav <nirav.sculptsoft@gmail.com>
2022-05-22 15:32:20 +0530

    > second commit

--- app/src/main/java/com/example/rebaseunderstanding2/MainActivity.kt
+++ app/src/main/java/com/example/rebaseunderstanding2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rebaseunderstanding2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 // first commit
+// second commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
